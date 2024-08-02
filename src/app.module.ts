f76ecbcdb4f1083b167944ecd092b5415aa74d31
mdcloud <mmd26.aka@gmail.com>
2024-08-02 18:39:13 +0000

    added jwt auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { databaseProviders } from './provider/database.provider';
 import { CustomerAddressesModule } from './customer_addresses/customer_addresses.module';
 import { CustomersModule } from './customers/customers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CustomerAddressesModule, CustomersModule],
+  imports: [CustomerAddressesModule, CustomersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, ...databaseProviders],
 })
