74f9b1fb702f48522defad25bbe165002ddbe037
mdcloud <mmd26.aka@gmail.com>
2024-08-02 17:24:01 +0000

    removed entities folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { RedisOptions } from './configs/app-options.configs';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }),
