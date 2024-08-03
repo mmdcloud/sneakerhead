e7515738f218734c08b6c4255e01f2f1d746d609
mdcloud <mmd26.aka@gmail.com>
2024-08-03 06:41:11 +0000

    remove customer and auth references

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseProviders } from './provider/database.provider';
 import { CustomerAddressesModule } from './customer_addresses/customer_addresses.module';
-import { CustomersModule } from './customers/customers.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CustomerAddressesModule, CustomersModule, AuthModule],
+  imports: [CustomerAddressesModule],
   controllers: [AppController],
   providers: [AppService, ...databaseProviders],
 })
