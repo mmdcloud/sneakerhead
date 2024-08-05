71a0673275fd6e1261cca1fc50bdc85c571f2882
mmdcloud <mmd26.aka@gmail.com>
2024-08-05 12:16:50 +0000

    added jwt config

--- src/customer_addresses/customer_addresses.module.ts
+++ src/customer_addresses/customer_addresses.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { CustomerAddressesService } from './customer_addresses.service';
 import { CustomerAddressesController } from './customer_addresses.controller';
 import { customerAddressProviders } from './provider/customer-addresses.provider';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './configs/constants';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [CustomerAddressesController],
-  providers: [CustomerAddressesService, ...customerAddressProviders],
+  providers: [
+    CustomerAddressesService,
+    ...customerAddressProviders,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class CustomerAddressesModule { }
