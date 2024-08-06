180f0fce400c88ab4b785542ee1e9fa376e0b343
mmdcloud <mmd26.aka@gmail.com>
2024-08-06 02:29:28 +0000

    added jwt config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { databaseProviders } from './provider/database.provider';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './configs/constants';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, ...databaseProviders],
+  providers: [
+    AppService,
+    ...databaseProviders,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 
 export class AppModule { }
