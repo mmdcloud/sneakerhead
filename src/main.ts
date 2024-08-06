936ed46b39e6155e2688d27cdab42e87c36ab4ea
mmdcloud <mmd26.aka@gmail.com>
2024-08-06 02:44:37 +0000

    added helmet and csrf config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
+  app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
